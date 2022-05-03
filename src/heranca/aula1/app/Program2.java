f97df4f1f857f7380ddc2e650352438d7e26294f
Urias01 <uriasluis64@gmail.com>
2022-05-03 19:23:39 -0300

    Aula herança

---
+++ src/heranca/aula1/app/Program2.java
@@ -0,0 +1,26 @@
+package heranca.aula1.app;
+
+import heranca.aula1.model.entities.Account;
+import heranca.aula1.model.entities.SavingsAccount;
+import heranca.aula1.model.entities.BusinessAccount;
+
+public class Program2 {
+    
+    public static void main(String[] args) {
+        
+    
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        acc1.withdraw(200.0);
+    
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+    }
+
+}
