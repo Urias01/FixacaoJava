617e92cefd3840ee5fda38169dd11d50f662b973
Urias01 <uriasluis64@gmail.com>
2022-05-04 16:34:03 -0300

    initial polimorfism

--- src/heranca/aula1/app/Program2.java
+++ src/heranca/aula1/app/Program2.java
@@ -1,26 +1,26 @@
 package heranca.aula1.app;
 
+import java.util.Scanner;
+
 import heranca.aula1.model.entities.Account;
 import heranca.aula1.model.entities.SavingsAccount;
-import heranca.aula1.model.entities.BusinessAccount;
 
 public class Program2 {
     
     public static void main(String[] args) {
+            
+            Scanner sc = new Scanner (System.in);
+            
+            Account x = new Account(1020, "Alex", 1000.0);
+            Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+            x.withdraw(50.0);
+            y.withdraw(50.0);
         
-    
-        Account acc1 = new Account(1001, "Alex", 1000.0);
-        acc1.withdraw(200.0);
-    
-        System.out.println(acc1.getBalance());
+            System.out.println(x.getBalance());
+            System.out.println(y.getBalance());
 
-        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-        acc2.withdraw(200.0);
-        System.out.println(acc2.getBalance());
-
-        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
+        sc.close();
     }
 
 }
